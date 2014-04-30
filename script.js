99cd79b59b32d99edef654bf28eb478d4c226db9
EncounterQ <EncounterQ@users.noreply.github.com>
2014-04-30 21:49:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.write('<p>Nothing much to see here...</p>');
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+canvas.width = 480;
+canvas.height = 640;
